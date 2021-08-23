9530842a652e884e9df41453954d35492bd1d518
mansouralbatran <mansouralbatran@gmail.com>
2021-08-23 20:48:42 +0300

    lab12

--- src/BestBooks.js
+++ src/BestBooks.js
@@ -2,8 +2,19 @@ import React from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Jumbotron from 'react-bootstrap/Jumbotron';
 import './BestBooks.css';
+import axios from 'axios';
+
 
 class MyFavoriteBooks extends React.Component {
+  constructor(props){
+    super()
+
+
+  };
+componentDidMount(){
+  let bookData =  axios.get(`${process.env.REACT_APP_SERVER}/getbook?useremail=`)
+
+}
   render() {
     return(
       <Jumbotron>
